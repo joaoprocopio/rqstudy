993bd3f2c8c170e83b3d4fb17c57c318c851a03c
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-10-28 19:27:42 -0300

    db shutdown

--- internal/server/logger.go
+++ internal/server/logger.go
@@ -8,11 +8,13 @@ import (
 type Logger struct {
 	Info  *log.Logger
 	Error *log.Logger
+	Fatal *log.Logger
 }
 
 func NewLogger() *Logger {
 	return &Logger{
 		Info:  log.New(os.Stdout, "info: ", log.LstdFlags),
 		Error: log.New(os.Stderr, "error: ", log.LstdFlags),
+		Fatal: log.New(os.Stderr, "fatal: ", log.LstdFlags),
 	}
 }
