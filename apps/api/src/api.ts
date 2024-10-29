e1dbe1d15d8e28989e461142f4ecea75878d581a
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-10-29 19:54:57 -0300

    api router

--- apps/api/src/api.ts
+++ apps/api/src/api.ts
@@ -1,7 +1,20 @@
 import fp from "fastify-plugin"
 
-export const api = fp((app) => {
-  app.get("/health", async (request, reply) => {
-    reply.send("ok")
+export const healthAPI = fp((app) => {
+  app.get("/health", (_, reply) => {
+    reply.status(200).send({
+      status: 200,
+      detail: "ok",
+    })
+  })
+})
+
+export const authAPI = fp((app) => {
+  app.get("/whoami", (_, reply) => {
+    reply.send({
+      id: 1,
+      name: "John Doe",
+      email: "johdoe@gmail.com",
+    })
   })
 })
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,13 +1,14 @@
 import { fastifyCors } from "@fastify/cors"
 import { fastify } from "fastify"
 
-import { api } from "./api"
+import { authAPI, healthAPI } from "./api"
 import { delayMiddleware } from "./middlewares"
 
 const app = fastify({ logger: { transport: { target: "pino-pretty" } } })
 
 await app.register(fastifyCors)
 await app.register(delayMiddleware)
-await app.register(api)
+await app.register(healthAPI, { prefix: "/health" })
+await app.register(authAPI, { prefix: "/api/auth" })
 
 app.listen({ host: "localhost", port: 8000 })
