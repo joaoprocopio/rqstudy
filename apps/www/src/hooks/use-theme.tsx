f55536e0d8193c398fd8ea9b987975ff18776e26
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-10-30 23:34:58 -0300

    dá uma limpada

--- apps/www/src/hooks/use-theme.tsx
+++ apps/www/src/hooks/use-theme.tsx
@@ -4,6 +4,7 @@ import { atomWithStorage } from "jotai/utils"
 type Theme = "dark" | "light" | "system"
 
 const baseThemeAtom = atomWithStorage<Theme>("theme", "system", undefined, { getOnInit: true })
+
 const themeAtom = atom(
   (get) => {
     return get(baseThemeAtom)
@@ -17,24 +18,19 @@ const themeAtom = atom(
     set(baseThemeAtom, nextTheme)
   },
 )
+
 themeAtom.onMount = (set) => {
   const store = getDefaultStore()
   const currTheme = store.get(themeAtom)
-
-  const rootEl = window.document.documentElement
-
-  rootEl.classList.remove("light", "dark")
 
   if (currTheme === "system") {
     const prefersDarkTheme: boolean = window.matchMedia("(prefers-color-scheme: dark)").matches
     const preferenceTheme: Theme = prefersDarkTheme ? "dark" : "light"
 
-    rootEl.classList.add(preferenceTheme)
     set(preferenceTheme)
     return
   }
 
-  rootEl.classList.add(currTheme)
   set(currTheme)
 }
 
